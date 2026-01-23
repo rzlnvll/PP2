102d80dfab40d0e40cde1cad8e632597c1e30c7e
rzlnvll <rozalinfcj@gmail.com>
2026-01-23 21:23:09 +0500

    adding numbers.py with type conversion

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,14 @@
+x = 1  
+y = 8.9 
+z = 2j  
+
+#convert from int to float:
+a = float(x)
+#convert from float to int:
+b = int(y)
+#convert from int to complex:
+c = complex(x)
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
